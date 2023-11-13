6c3613da85b278441641ca6c493f175f0ea5ef0f
Luu Duc Dong <luuducdong@Ludo-Macbook-Pro.local>
2023-11-13 11:40:18 +0700

    feat(block-information): add number value

--- lib/src/core/block_information.dart
+++ lib/src/core/block_information.dart
@@ -4,23 +4,24 @@ import 'package:web3dart/web3dart.dart';
 class BlockInformation {
   final EtherAmount? baseFeePerGas;
   final DateTime timestamp;
+  final BigInt? number;
 
-  BlockInformation({
-    required this.baseFeePerGas,
-    required this.timestamp,
-  });
+  BlockInformation(
+      {required this.baseFeePerGas, required this.timestamp, this.number});
 
   factory BlockInformation.fromJson(Map<String, dynamic> json) {
     return BlockInformation(
-      baseFeePerGas: json.containsKey('baseFeePerGas')
-          ? EtherAmount.fromUnitAndValue(
-              EtherUnit.wei, hexToInt(json['baseFeePerGas'] as String))
-          : null,
-      timestamp: DateTime.fromMillisecondsSinceEpoch(
-        hexToDartInt(json['timestamp'] as String) * 1000,
-        isUtc: true,
-      ),
-    );
+        baseFeePerGas: json.containsKey('baseFeePerGas')
+            ? EtherAmount.fromUnitAndValue(
+                EtherUnit.wei, hexToInt(json['baseFeePerGas'] as String))
+            : null,
+        timestamp: DateTime.fromMillisecondsSinceEpoch(
+          hexToDartInt(json['timestamp'] as String) * 1000,
+          isUtc: true,
+        ),
+        number: json['number'] != null && json['number'] is String
+            ? BigInt.tryParse(json['number'] as String)
+            : null);
   }
 
   bool get isSupportEIP1559 => baseFeePerGas != null;
